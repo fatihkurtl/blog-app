ba831ff1e39df39b34b53a15f9f21a065c2e8828
fatihkurtl <kurtf061@gmail.com>
2023-11-17 15:13:39 +0300

    email-subscribe api changed

--- backend/subscribe_emails/urls.py
+++ backend/subscribe_emails/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.email_subscribe, name='email-subscribe'),
+    path('', views.EmailSubscribeViewSet.as_view(), name='email-subscribe'),
 ]
--- backend/subscribe_emails/views.py
+++ backend/subscribe_emails/views.py
@@ -7,21 +7,44 @@ from member.models import MEMBER
 from .models import SubscribeVisitors
 from .serializers import SubscribeVisitorsSerializer
 
+from rest_framework import generics
 
-@api_view(['PUT'])
-def email_subscribe(request):
-  """Subscribes a user to the newsletter."""
 
-  email = request.data["email"]
+class EmailSubscribeViewSet(generics.RetrieveAPIView):
+  
+  def post(self, request, *args, **kwargs):
+    email = request.data['email']
+    
+    member = MEMBER.objects.filter(email=email).first()
+    visitor = SubscribeVisitors.objects.filter(visitor_email=email).first()
+    
+    if member is not None and member.email == request.data['email']:
+      if member.email_subscribe == True:
+        return Response({'message': 'You are already subscribed'}, status.HTTP_400_BAD_REQUEST)
+      member.email_subscribe = True
+      member.save()
+      return Response({'message': 'Subscribed'}, status.HTTP_202_ACCEPTED)
+    
+    visitor = SubscribeVisitors(visitor_email=email)
+    print(visitor.visitor_email)
+    visitor.save()
+    return Response({'message': 'Subscribed'}, status.HTTP_202_ACCEPTED)
 
-  member = MEMBER.objects.filter(email=email).first()
-  visitor = SubscribeVisitors.objects.filter(visitor_email=email).first()
 
-  if member is not None and member.email == request.data['email']:
-    member.email_subscribe = True
-    member.save()
-    return Response({'message': 'Subscribed', 'status': status.HTTP_202_ACCEPTED}, status.HTTP_202_ACCEPTED)
+# @api_view(['PUT'])
+# def email_subscribe(request):
+#   """Subscribes a user to the newsletter."""
 
-  visitor = SubscribeVisitors(visitor_email=email)
-  visitor.save()
-  return Response({'message': 'Subscribed', 'status': status.HTTP_202_ACCEPTED}, status.HTTP_202_ACCEPTED)
+#   email = request.data["email"]
+
+#   member = MEMBER.objects.filter(email=email).first()
+#   visitor = SubscribeVisitors.objects.filter(visitor_email=email).first()
+
+#   if member is not None and member.email == request.data['email']:
+#     member.email_subscribe = True
+#     member.save()
+#     return Response({'message': 'Subscribed', 'status': status.HTTP_202_ACCEPTED}, status.HTTP_202_ACCEPTED)
+
+#   visitor = SubscribeVisitors(visitor_email=email)
+#   visitor.save()
+#   return Response({'message': 'Subscribed', 'status': status.HTTP_202_ACCEPTED}, status.HTTP_202_ACCEPTED)
